042e64d6d1dfedadc07e965a2f5f0738b6b9ed7b
anastasiiaboiko <anastacia.boiko@gmail.com>
2015-01-03 18:48:16 +0200

    Added labels and textfields
    Added labels and textfields for info about new item

--- HomePwner/BNRDetailViewController.m
+++ HomePwner/BNRDetailViewController.m
@@ -10,6 +10,11 @@
 
 @interface BNRDetailViewController ()
 
+@property (weak, nonatomic) IBOutlet UITextField *nameField;
+@property (weak, nonatomic) IBOutlet UITextField *serialNumberField;
+@property (weak, nonatomic) IBOutlet UITextField *valueField;
+@property (weak, nonatomic) IBOutlet UILabel *dateLabel;
+
 @end
 
 @implementation BNRDetailViewController
