beb9a724306e6e4963a36ad8644724247371c3a1
anastasiiaboiko <anastacia.boiko@gmail.com>
2015-01-03 18:23:49 +0200

    Added DetailViewController

---
+++ HomePwner/BNRDetailViewController.m
@@ -0,0 +1,17 @@
+//
+//  BNRDetailViewController.m
+//  HomePwner
+//
+//  Created by Anastasia Boiko on 1/3/15.
+//  Copyright (c) 2015 Big Nerd Ranch. All rights reserved.
+//
+
+#import "BNRDetailViewController.h"
+
+@interface BNRDetailViewController ()
+
+@end
+
+@implementation BNRDetailViewController
+
+@end
